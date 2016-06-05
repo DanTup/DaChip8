9c8f61e36919e4c9f0ff1b4bd639de641600180d
Danny Tuppeny <danny@tuppeny.com>
2016-06-05 13:25:00 +0100

    Change V registers to array so we can index into them.

--- DaChip8/Chip8.cs
+++ DaChip8/Chip8.cs
@@ -9,7 +9,7 @@ namespace DanTup.DaChip8
 		Bitmap screen; // 64x32
 
 		// Registers
-		byte V0, V1, V2, V3, V4, V5, V6, V7, V8, V9, VA, VB, VC, VD, VE, VF;
+		byte[] V = new byte[16];
 		// Timers
 		byte Delay, Sound;
 		// Address/Program Counters
