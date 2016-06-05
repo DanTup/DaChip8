89ed366b8243e7fe68a1f7ecc0df67dbc501ca5b
Danny Tuppeny <danny@tuppeny.com>
2016-06-05 11:45:13 +0100

    Added class for main interpreter.

---
+++ DaChip8/Chip8.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace DanTup.DaChip8
+{
+	class Chip8
+	{
+		Bitmap screen; // 64x32
+
+		public Chip8(Bitmap screen)
+		{
+			this.screen = screen;
+		}
+	}
+}
--- DaChip8/Screen.cs
+++ DaChip8/Screen.cs
@@ -1,12 +1,20 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
 
 namespace DanTup.DaChip8
 {
 	public partial class Screen : Form
 	{
+		Chip8 chip8;
+		Bitmap screen;
+
 		public Screen()
 		{
 			InitializeComponent();
+
+			screen = new Bitmap(64, 32, PixelFormat.Format1bppIndexed);
+			chip8 = new Chip8(screen);
 		}
 	}
 }
