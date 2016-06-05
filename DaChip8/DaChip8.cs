30fd84db01feda7222f9447b8014f9be8acda08e
Danny Tuppeny <danny@tuppeny.com>
2016-06-05 11:21:57 +0100

    Added empty WinForms project.

---
+++ DaChip8/DaChip8.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace DanTup.DaChip8
+{
+	public partial class DaChip8 : Form
+	{
+		public DaChip8()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ DaChip8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DanTup.DaChip8
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new DaChip8());
+		}
+	}
+}
