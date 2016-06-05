f0706d6d22c80417dd142201f51267538824fddd
Danny Tuppeny <danny@tuppeny.com>
2016-06-05 12:05:55 +0100

    Add variables for all registers and a Tick placeholder.

--- DaChip8/Chip8.cs
+++ DaChip8/Chip8.cs
@@ -6,9 +6,20 @@ namespace DanTup.DaChip8
 	{
 		Bitmap screen; // 64x32
 
+		// Registers
+		byte V0, V1, V2, V3, V4, V5, V6, V7, V8, V9, VA, VB, VC, VD, VE, VF;
+		byte SP, Delay, Sound;
+		ushort I, PC;
+		ushort[] Stack;
+
 		public Chip8(Bitmap screen)
 		{
 			this.screen = screen;
+		}
+
+		public void Tick()
+		{
+			
 		}
 	}
 }
