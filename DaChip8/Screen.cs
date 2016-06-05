34b0a6d622b37adc20121d60ad5a3340e4580967
Danny Tuppeny <danny@tuppeny.com>
2016-06-05 12:06:08 +0100

    Add a 60Hz loop.

--- DaChip8/Screen.cs
+++ DaChip8/Screen.cs
@@ -1,4 +1,6 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
 
@@ -6,8 +8,13 @@ namespace DanTup.DaChip8
 {
 	public partial class Screen : Form
 	{
-		Chip8 chip8;
-		Bitmap screen;
+		readonly Chip8 chip8;
+		readonly Bitmap screen;
+
+		// For timing..
+		readonly Stopwatch stopWatch = Stopwatch.StartNew();
+		readonly TimeSpan targetElapsedTime = TimeSpan.FromTicks(TimeSpan.TicksPerSecond / 60);
+		TimeSpan lastTime;
 
 		public Screen()
 		{
@@ -15,6 +22,28 @@ namespace DanTup.DaChip8
 
 			screen = new Bitmap(64, 32, PixelFormat.Format1bppIndexed);
 			chip8 = new Chip8(screen);
+
+			Application.Idle += IdleTick;
+		}
+
+		void IdleTick(object sender, EventArgs e)
+		{
+			var currentTime = stopWatch.Elapsed;
+			var elapsedTime = currentTime - lastTime;
+
+			while (elapsedTime >= targetElapsedTime)
+			{
+				Tick();
+				elapsedTime -= targetElapsedTime;
+				lastTime += targetElapsedTime;
+			}
+
+			Invalidate();
+		}
+
+		void Tick()
+		{
+			chip8.Tick();
 		}
 	}
 }
