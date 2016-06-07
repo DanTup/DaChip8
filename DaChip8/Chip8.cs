8cdc62969d842e7de09bcde1ba45fa3fc6364606
Danny Tuppeny <danny@tuppeny.com>
2016-06-07 18:03:23 +0100

    Set NN!

--- DaChip8/Chip8.cs
+++ DaChip8/Chip8.cs
@@ -91,10 +91,11 @@ namespace DanTup.DaChip8
 			var op = new OpCodeData()
 			{
 				OpCode = opCode,
-				NNN = (ushort)(opCode & 0x00FFF),
+				NNN = (ushort)(opCode & 0x0FFF),
+				NN = (byte)(opCode & 0x00FF),
+				N = (byte)(opCode & 0x000F),
 				X = (byte)(opCode & 0x0F00 >> 8),
 				Y = (byte)(opCode & 0x00F0 >> 4),
-				N = (byte)(opCode & 0x000F)
 			};
 
 			// Loop up the OpCode using the first nibble and execute.
