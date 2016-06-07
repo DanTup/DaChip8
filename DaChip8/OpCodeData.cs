9d01ceb868c7c511248eb5be08dee2a9b916366a
Danny Tuppeny <danny@tuppeny.com>
2016-06-07 18:32:16 +0100

    Added ToString to aid debugging.

--- DaChip8/OpCodeData.cs
+++ DaChip8/OpCodeData.cs
@@ -5,5 +5,10 @@
 		public ushort OpCode;
 		public ushort NNN;
 		public byte NN, X, Y, N;
+
+		public override string ToString()
+		{
+			return $"{OpCode:X4} (X: {X:X}, Y: {Y:X}, N: {N:X}, NN: {NN:X:X2}, NNN: {NNN:X3})";
+		}
 	}
 }
