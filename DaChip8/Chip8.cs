5013fa9892a115313a1ab0dd26267a235ee0ba52
Danny Tuppeny <danny@tuppeny.com>
2016-06-07 18:36:34 +0100

    Fix precedence bug.

--- DaChip8/Chip8.cs
+++ DaChip8/Chip8.cs
@@ -94,8 +94,8 @@ namespace DanTup.DaChip8
 				NNN = (ushort)(opCode & 0x0FFF),
 				NN = (byte)(opCode & 0x00FF),
 				N = (byte)(opCode & 0x000F),
-				X = (byte)(opCode & 0x0F00 >> 8),
-				Y = (byte)(opCode & 0x00F0 >> 4),
+				X = (byte)((opCode & 0x0F00) >> 8),
+				Y = (byte)((opCode & 0x00F0) >> 4),
 			};
 
 			// Loop up the OpCode using the first nibble and execute.
